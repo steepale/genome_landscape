7d4a8654caccd8899a66271a9db81658a2f14201
steepale <steepale@msu.edu>
2017-05-03 20:45:55 +0200

    updated scripts for filtering all somatic snvs and indels with custom filter method

---
+++ scripts/custom_filter_somatic_snvs_indels_wrapper.sh
@@ -0,0 +1,25 @@
+
+cd /Users/Alec/Documents/Bioinformatics/MDV_Project/genome_landscape
+
+# Assign the sample variable
+Tbird=$1
+
+# Combine the SNV and INDEL files
+(grep "^#" '/home/proj/MDW_genomics/steepale/mutated_gene_annotation/data/somaticseq_vcf/'$Tbird'_somaticseq_snv_vep.vcf'; \
+grep -v "^#" '/home/proj/MDW_genomics/steepale/mutated_gene_annotation/data/somaticseq_vcf/'$Tbird'_somaticseq_snv_vep.vcf'; \
+grep -v "^#" '/home/proj/MDW_genomics/steepale/mutated_gene_annotation/data/somaticseq_vcf/'$Tbird'_somaticseq_indel_vep.vcf') > \
+'/home/proj/MDW_genomics/steepale/mutated_gene_annotation/data/somaticseq_vcf/'$Tbird'_somaticseq_snv_indel_vep.vcf'
+
+# Create an empty file or python script will error
+touch './data/all_somatic_snv_indel_'$Tbird'.txt'
+
+# Run python filtering script
+python ./scripts/custom_filter_somatic_snvs_indels.py \
+'/home/proj/MDW_genomics/steepale/mutated_gene_annotation/data/somaticseq_vcf/'$Tbird'_somaticseq_snv_indel_vep.vcf' \
+'./data/all_somatic_snv_indel_'$Tbird'.txt' \
+$Tbird
+
+# Sort the sites that passed filtering to reduce redundency
+(grep "^#" './data/all_somatic_snv_indel_'$Tbird'.txt'; \
+grep -v "^#" './data/all_somatic_snv_indel_'$Tbird'.txt' | sort | uniq) > \
+'./data/all_somatic_snv_indel_'$Tbird'_final.txt'
